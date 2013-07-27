ed0317dc82f3b0ba31f15ace1966932590dcb5bf
Jeremie Lagarde <jer@printstacktrace.org>
2013-07-27 09:09:44 +0200

    Managing defaultProtocol as "Servlet 3.0" in arquillian.xml file.

--- src/main/java/org/jboss/forge/arquillian/extension/warp/WarpPlugin.java
+++ src/main/java/org/jboss/forge/arquillian/extension/warp/WarpPlugin.java
@@ -1,6 +1,10 @@
 package org.jboss.forge.arquillian.extension.warp;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
@@ -10,9 +14,13 @@ import org.jboss.forge.arquillian.extension.drone.DroneFacet;
 import org.jboss.forge.parser.java.JavaClass;
 import org.jboss.forge.parser.java.JavaSource;
 import org.jboss.forge.parser.java.Method;
+import org.jboss.forge.parser.xml.Node;
+import org.jboss.forge.parser.xml.XMLParser;
 import org.jboss.forge.project.Project;
 import org.jboss.forge.project.facets.JavaSourceFacet;
+import org.jboss.forge.project.facets.ResourceFacet;
 import org.jboss.forge.project.facets.events.InstallFacets;
+import org.jboss.forge.resources.FileResource;
 import org.jboss.forge.resources.Resource;
 import org.jboss.forge.resources.java.JavaResource;
 import org.jboss.forge.shell.Shell;
@@ -52,7 +60,7 @@ public class WarpPlugin implements Plugin
    @Inject
    @Current
    private Resource<?> currentResource;
-   
+
    @Inject
    private Shell shell;
 
@@ -68,10 +76,49 @@ public class WarpPlugin implements Plugin
       {
          ShellMessages.success(out, "Arquillian Warp extension is installed.");
       }
+
+      ResourceFacet resources = project.getFacet(ResourceFacet.class);
+      FileResource<?> resource = (FileResource<?>) resources.getTestResourceFolder().getChild("arquillian.xml");
+
+      if (!resource.exists())
+      {
+         ShellMessages.warn(out, "arquillian.xml file could not be found!");
+      }
+      else
+      {
+         final Node xml = XMLParser.parse(resource.getResourceInputStream());
+         Node defaultProtocol = xml.getSingle("defaultProtocol");
+         if (defaultProtocol == null || !"Servlet 3.0".equals(defaultProtocol.getAttribute("type")))
+         {
+            if (shell.promptBoolean(
+                     "\"Servlet 3.0\" protocol is required in the arquillian.xml file, do you want to add id?", true))
+            {
+               final Node node = xml.getOrCreate("defaultProtocol@type=Servlet 3.0");
+               //TODO : litle hack to wait FORGE-998
+               Node root = new Node(xml.getName()) {
+
+                  @Override
+                  public Map<String, String> getAttributes()
+                  {
+                     return xml.getAttributes();
+                  }
+                  
+                  @Override
+                  public List<Node> getChildren()
+                  {
+                     List<Node> children =  new ArrayList<Node>(xml.getChildren());
+                     children.remove(node);
+                     children.add(0,node);
+                     return Collections.unmodifiableList(children);
+                  }
+               };
+               resource.setContents(XMLParser.toXMLString(root));
+            }
+         }
+      }
    }
 
-
-   @Command(value = "usingWarp", help = "Adding Wrap to a test.")
+   @Command(value = "usingWarp", help = "Adding Wrap to a test method.")
    public void newElement(@Option(name = "test", completer = TestMethodCompleter.class) final String test,
             final PipeOut out)
             throws Exception
@@ -79,7 +126,7 @@ public class WarpPlugin implements Plugin
       final JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
 
       JavaClass javaClass = getJavaClass();
-      
+
       if (!javaClass.hasMethodSignature(test))
       {
          throw new IllegalStateException("Class does not have a method named [" + test + "]");
@@ -91,7 +138,7 @@ public class WarpPlugin implements Plugin
       }
 
       Method<JavaClass> method = javaClass.getMethod(test);
-      
+
       if (!javaClass.hasAnnotation("org.jboss.arquillian.container.test.api.RunAsClient")
                && !method.hasAnnotation("org.jboss.arquillian.container.test.api.RunAsClient"))
       {
@@ -101,26 +148,27 @@ public class WarpPlugin implements Plugin
       javaClass.addImport("org.jboss.arquillian.warp.Inspection");
       javaClass.addImport("org.jboss.arquillian.warp.Warp");
       javaClass.addImport("org.jboss.arquillian.warp.servlet.BeforeServlet");
-      
+
       StringBuilder body = new StringBuilder(method.getBody());
       body.append("Warp\n")
-      .append("      .initiate(new Activity() {\n")
-      .append("                public void perform() {\n")
-      .append("              // TODO : Add activity (ex : browser.navigate().to(contextPath + \"index.html\");)\n")
-      .append("          }\n")
-      .append("      })\n")
-      .append("      .inspect(new Inspection() {\n")
-      .append("           private static final long serialVersionUID = 1L;\n")
-      .append("\n")
-      .append("          @BeforeServlet\n")
-      .append("          public void beforeServlet() {\n")
-      .append("              System.out.println(\"Hi server! : Message injected by Arquillian Warp!\");\n")
-      .append("          }\n")
-      .append("      });\n");
+               .append("      .initiate(new Activity() {\n")
+               .append("                public void perform() {\n")
+               .append("              // TODO : Add activity (ex : browser.navigate().to(contextPath + \"index.html\");)\n")
+               .append("          }\n")
+               .append("      })\n")
+               .append("      .inspect(new Inspection() {\n")
+               .append("           private static final long serialVersionUID = 1L;\n")
+               .append("\n")
+               .append("          @BeforeServlet\n")
+               .append("          public void beforeServlet() {\n")
+               .append("              System.out.println(\"Hi server! : Message injected by Arquillian Warp!\");\n")
+               .append("          }\n")
+               .append("      });\n");
       method.setBody(body.toString());
       java.saveTestJavaSource(javaClass);
 
    }
+
    private JavaClass getJavaClass() throws FileNotFoundException
    {
       Resource<?> resource = shell.getCurrentResource();
